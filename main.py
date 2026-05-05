295bfa1ef03ca094231a6e1b3ab952c8f84fb03e
Ysllanos <yoinermoncadallanos@gmail.com>
2026-05-04 19:37:37 -0500

    fix: universal CORS permissions for Vercel previews

--- main.py
+++ main.py
@@ -61,11 +61,12 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 
 # Montar archivos estáticos para las imágenes (Ruta Relativa)
